4b5f474c4001fbbe4f53227d70ec770e2a3ecf21
Poukk <alexandrefilho.06@hotmail.com>
2025-01-05 22:49:58 -0300

    incrementing navbar: making work and adding prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://poukkin.me/',
-  integrations: [tailwind()],
+	site: 'https://poukkin.me/',
+	integrations: [tailwind()],
+	prefetch: true,
 });
